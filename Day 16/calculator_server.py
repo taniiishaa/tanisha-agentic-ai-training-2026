756751dbbb14b21c0142d7ef513da5000fa0d32a
Tanisha <tanishac216@gmail.com>
2026-07-02 21:25:08 +0530

    Update calculator_server.py

--- Day 16/calculator_server.py
+++ Day 16/calculator_server.py
@@ -4,27 +4,27 @@ from fastmcp import FastMCP
 mcp = FastMCP("Calculator")
 
 @mcp.tool
-def hello() -> str:
+def hello():
     """Returns a simple greeting."""
     return "Hello, World!"
 
 @mcp.tool
-def add(x: int, y: int) -> int:
+def add(x: int, y: int):
     """Adds two integers together."""
     return x + y
 
 @mcp.tool
-def subs(x: int, y: int) -> int:
+def subs(x: int, y: int):
     """Subtracts y from x."""
     return x - y
 
 @mcp.tool
-def mul(x: int, y: int) -> int:
+def mul(x: int, y: int):
     """Multiplies two integers."""
     return x * y
 
 @mcp.tool
-def div(x: int, y: int) -> float:
+def div(x: int, y: int):
     """Divides x by y."""
     if y == 0:
         raise ValueError("Cannot divide by zero!")
